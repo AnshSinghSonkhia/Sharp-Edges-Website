18a5cf499e8bf23ef0e1bb4bd828ed0231d37a29
Ansh Singh Sonkhia <anshssonkhia@gmail.com>
2022-10-28 19:17:01 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+/**
+ * add event on element (MACHINE)
+ */
+
+ const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+      for (let i = 0; i < elem.length; i++) {
+        elem[i].addEventListener(type, callback);
+      }
+    } else {
+      elem.addEventListener(type, callback);
+    }
+  }
+
+/**
+ * The Working of the Hamburger
+ */
+
+const HamburgerIcon = document.querySelector("[data-hamburger-icon]");
+const HamburgerList = document.querySelector("[data-hamburger-list]");
+
+var HamburgerClicks = 0;
+var HamburgerTouch = 0;
+
+HamburgerIcon.addEventListener("click", () => {
+    HamburgerClicks += 1;
+    console.log(HamburgerClicks);
+
+    //check if the number is even
+    if(HamburgerClicks % 2 == 0) {
+        console.log("The click is even.");
+        HamburgerList.classList.add("hidden");
+        HamburgerList.classList.remove("block");
+    }
+
+    // if the number is odd
+    else {
+        console.log("The click is odd.");
+        HamburgerList.classList.remove("hidden");
+        HamburgerList.classList.add("block");
+    }
+})
+
+HamburgerIcon.addEventListener("touchstart", () => {
+  HamburgerTouch += 1;
+    console.log(HamburgerTouch);
+
+    //check if the number is even
+    if(HamburgerTouch % 2 == 0) {
+        console.log("The touch is even.");
+        HamburgerList.classList.add("hidden");
+        HamburgerList.classList.remove("block");
+    }
+
+    // if the number is odd
+    else {
+        console.log("The touch is odd.");
+        HamburgerList.classList.remove("hidden");
+        HamburgerList.classList.add("block");
+    }
+})
+
+
+
+/**
+ * Footer
+ */
+
+//  let auto = document.getElementById("auto");
+//  let light = document.getElementById("light");
+//  let dark = document.getElementById("dark");
+//  const switchMode = (event) => {
+//      switch (event.target.value) {
+//          case "auto":
+//              auto.classList.remove("hidden");
+//              light.classList.add("hidden");
+//              dark.classList.add("hidden");
+//              break;
+//          case "light":
+//              auto.classList.add("hidden");
+//              light.classList.remove("hidden");
+//              dark.classList.add("hidden");
+//              break;
+//          case "dark":
+//              auto.classList.add("hidden");
+//              light.classList.add("hidden");
+//              dark.classList.remove("hidden");
+//              break;
+//          default:
+//              break;
+//      }
+//  };
+ 
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      animation: {
+        'slide-bl': 'slide-bl 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+        'slide-b': 'slide-bottom 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+        'slow-bounce': 'bounce 2.5s infinite',
+        'rotate-scale-down': 'rotate-scale-down 1.65s linear both infinite',
+        'rotate-scale-down-2': 'rotate-scale-down 2.65s linear both infinite',
+        'scale-up-center': 'scale-up-center 2.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both infinite',
+        'scale-down-center': 'scale-up-center 5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both infinite',
+        'tracking-in-expand': 'tracking-in-expand 2s cubic-bezier(0.215, 0.610, 0.355, 1.000) both',
+        'tracking-in-contract-bck': 'tracking-in-contract-bck 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both',
+        'heartbeat': 'heartbeat 1.5s ease-in-out infinite both',
+      }
+    },
+  },
+  plugins: [],
+}
