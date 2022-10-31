d49a85bc9b9479193c0a86fe1093744702f80240
Ansh Singh Sonkhia <anshssonkhia@gmail.com>
2022-10-31 20:03:27 +0530

    -

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: [
+    './index.html',
+    './404.html',
+    './pages/**/*.{html,js}',
+  ],
   theme: {
     extend: {
       animation: {
